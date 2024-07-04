de18a3454c975aafad3546f6daa14528bfc050fe
Ian Kinyanjui <99899168+iankinyanjuii@users.noreply.github.com>
2024-07-04 23:40:44 +0300

    github login test - cypress

---
+++ testgitlogin.cy.js
@@ -0,0 +1,31 @@
+describe('GitHub Login Automation', () => {
+  it('should log in successfully', () => {
+    cy.visit('https://github.com/login');
+
+    cy.get('input[name="login"]').type('githubtester003@gmail.com');
+    cy.get('input[name="password"]').type('L10n5d3n!');
+    cy.get('input[type="submit"]').click();
+
+    cy.url().should('include', '/github.com');
+  });
+
+  it('should handle invalid credentials', () => {
+    cy.visit('https://github.com/login');
+
+    cy.get('input[name="login"]').type('githubtester003@gmail.com');
+    cy.get('input[name="password"]').type('invalid_password');
+    cy.get('input[type="submit"]').click();
+
+    cy.get('#js-flash-container').should('contain', 'Incorrect username or password.');
+  });
+
+  it('should handle missing password', () => {
+    cy.visit('https://github.com/login');
+
+    cy.get('input[name="login"]').type('githubtester003@gmail.com');
+    cy.get('input[name="password"]').clear();
+    cy.get('input[type="submit"]').click();
+
+    cy.get('#js-flash-container').should('contain', 'Password is required.');
+  });
+});
